c3de669c8d6fff880e8ffbcc4a7a04ddf3d934c2
Nicolas Martinez <Nimarti548@gmail.com>
2021-01-02 18:11:25 -0800

    UPDATE: added switch case to server

--- server.js
+++ server.js
@@ -17,3 +17,29 @@ const menu = () => {
             "exit"
         ]
     })
+    .then(({ menuOptions }) => {
+        switch (menuOptions) {
+            case 'View Departments':
+                viewDepartment()
+                break;
+            case 'View Roles':
+                viewRoles()
+                break;
+            case 'View Employees':
+                viewEmployees()
+                break;
+            case 'Add Department':
+                addDepartment()
+                break;
+            case 'Add Role':
+                addRole()
+                break;
+            case 'Add Employee':
+                addEmployee()
+                break;
+            default:
+                connection.end()
+                process.exit(0)
+        }
+    })
+}
